266fe19c80cf441a85a774afcdbaaa83f897ff80
zhuyixin <zhu_yixin@qq.com>
2019-05-27 18:46:25 +0800

    init“

---
+++ app/src/main/java/io/agora/rtc/mediaio/app/InitActivity.java
@@ -0,0 +1,46 @@
+package io.agora.rtc.mediaio.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import io.agora.rtc.mediaio.app.videoSource.ui.MainActivity;
+import io.agora.rtc.mediaio.app.shareScreen.ui.ShareScreenActivity;
+
+public class InitActivity extends Activity {
+
+    private static final String LOG_TAG = "InitActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_activity);
+
+    }
+
+
+    public void onOldInterfaceClicked(View view) {
+        Intent intent1 = new Intent(InitActivity.this,
+                MainActivity.class);
+        startActivity(intent1);
+    }
+
+
+    public void onNewInterfaceClicked(View view) {
+        Intent intent1 = new Intent(InitActivity.this,
+                ShareScreenActivity.class);
+        startActivity(intent1);
+    }
+
+    public void onRecordClick(View view) {
+        startActivity(new Intent(this, RecordActivity.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
